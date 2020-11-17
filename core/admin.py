87bde0152456b02deb7c1fc8c973cc0056a73cc1
JeoftonCosta <jeofton_costa@hotmail.com>
2020-11-17 20:58:41 -0300

    Primeiro commit - adição do projeto

---
+++ core/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+
+from .models import Contato, Categoria
+
+class ContatoAdmin(admin.ModelAdmin):
+    list_display = ('nome','sobrenome','telefone','descricao')
+
+admin.site.register(Contato, ContatoAdmin)
+admin.site.register(Categoria)
---
+++ core/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render, get_object_or_404
+from django.http import Http404
+from .models import Contato
+
+
+def index(request):
+    contatos = Contato.objects.all()
+    return render(request, 'core/index.html', {'contatos': contatos})
+
+
+def ver_contato(request, contato_id):
+    contato = get_object_or_404(Contato, id=contato_id)
+    return render(request, 'core/ver_contato.html', {'contato': contato})
+    
